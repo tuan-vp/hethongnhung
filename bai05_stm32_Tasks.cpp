aede4f54aa8635a907ce7534401a03d4cd463d9e
tuan-vp <tuanvp.research@gmail.com>
2026-01-25 23:38:19 +0700

    Create bai05_stm32_Tasks.cpp

---
+++ bai05_stm32_Tasks.cpp
@@ -0,0 +1,131 @@
+#include "main.h"
+#include "cmsis_os.h"
+
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+void HTN_B05_LedPB05(void * arg);
+void HTN_B05_LedPB06(void * arg);
+void HTN_B05_LedPB07(void * arg);
+void HTN_B05_LedPC13(void * arg);
+
+int main(void)
+{
+  HAL_Init();
+  SystemClock_Config();
+  MX_GPIO_Init();
+	
+	xTaskCreate(	HTN_B05_LedPC13,"Led PC13",128, NULL,
+								tskIDLE_PRIORITY + 1, NULL);
+  xTaskCreate(	HTN_B05_LedPB05,"Led PB05",128, NULL,
+								tskIDLE_PRIORITY + 1, NULL);
+	
+	vTaskStartScheduler();
+  while (1)
+  {
+  }
+}
+
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7|GPIO_PIN_5|GPIO_PIN_6, GPIO_PIN_RESET);
+
+  GPIO_InitStruct.Pin = GPIO_PIN_13;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  GPIO_InitStruct.Pin =GPIO_PIN_5|GPIO_PIN_6| GPIO_PIN_7;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+}
+
+void HTN_B05_LedPB05(void * argument)
+{
+  for(int i=0;i<3;i++)
+  {
+		HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_5);
+    vTaskDelay(pdMS_TO_TICKS(500));
+  }
+	xTaskCreate(	HTN_B05_LedPB06,"Led PB06",128, NULL,
+								tskIDLE_PRIORITY + 1, NULL);
+	vTaskDelete(NULL);
+}
+void HTN_B05_LedPB06(void * argument)
+{
+  for(int i=0;i<3;i++)
+  {
+		HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_6);
+    vTaskDelay(pdMS_TO_TICKS(500));
+  }
+	xTaskCreate(	HTN_B05_LedPB07,"Led PB07",128, NULL,
+							tskIDLE_PRIORITY + 1, NULL);
+	vTaskDelete(NULL);
+}
+void HTN_B05_LedPB07(void * argument)
+{
+  for(int i=0;i<3;i++)
+  {
+		HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_7);
+    vTaskDelay(pdMS_TO_TICKS(500));
+  }
+	xTaskCreate(	HTN_B05_LedPB05,"Led PB05",128, NULL,
+								tskIDLE_PRIORITY + 1, NULL);
+	vTaskDelete(NULL);
+}
+void HTN_B05_LedPC13(void * argument)
+{
+  for(;;)
+  {
+		HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+    vTaskDelay(pdMS_TO_TICKS(500));
+  }
+}
+
+void Error_Handler(void)
+{
+  __disable_irq();
+  while (1)
+  {
+  }
+}
+
+#ifdef  USE_FULL_ASSERT
+void assert_failed(uint8_t *file, uint32_t line)
+{
+}
+#endif /* USE_FULL_ASSERT */
